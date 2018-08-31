42bb1b1e03fc60fa59f30a6efe5bf61e470930d8
Atguard <42866497+Atguard@users.noreply.github.com>
2018-08-31 16:31:03 +0200

    whatever

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,25 +1,52 @@
 <template>
   <div class="Test">
     <h1>{{ msg }}</h1>
-    <h2>h2Test ... </h2>
+    Hallo{{ user && ' '+user }}! Am {{date}} begrüße ich dich!
+    <br />
+    <input v-model="user" />
+    <button @click="refresh2">Refresch</button>
   </div>
 </template>
 
 <script>
+
 export default {
-  components: {},
-  name: "HelloWorld",
+  components: {
+  },
+  name: "Banner",
   data() {
     return {
-      msg: "Welcome to ... what ever!"
+      msg: "Welcome to ... what ever!",
+      date: (new Date()).toLocaleString(),
+      user: ""
     };
+  },
+  methods: {
+    refresh2: function() {
+      this.user = ""
+      this.refresh()
+    },
+    refresh: function() {
+      this.date = (new Date()).toLocaleString()
+    }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<!--
+Some colors:
+00B28B
+04FFC7
+00FFC6
+B22D00
+FF4100
+-->
 <style scoped>
-h1,
+h1 {
+  color: #B22D00;
+}
 h2 {
   font-weight: bold;
 }
